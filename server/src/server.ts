a07782925b4a9453c26ff515f34fef67f8d197ba
Yuri Canuto <yuriasc@gmail.com>
2020-09-01 01:37:41 -0300

    create database connection server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("UOOWWWWWW")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.send("TESTE");
+})
+
+app.listen(3333);
